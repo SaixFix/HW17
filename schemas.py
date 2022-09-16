82e00172919fe9a02765c3c75d9f18f6f4ceda4f
SaixFix <saixgs@gmail.com>
2022-09-16 15:46:53 +0300

    first commite, starting work, all Schemas done

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields
+
+from config import db
+
+
+class DirectorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class GenreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre = fields.Nested(GenreSchema, only=["name"])
+    director = fields.Nested(DirectorSchema, only=["name"])
+
+
