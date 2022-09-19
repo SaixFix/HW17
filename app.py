6bc905b14b53345551423a0e4c57efaeea877fb8
SaixFix <saixgs@gmail.com>
2022-09-19 19:20:22 +0300

    done: all schemas, all models, and few views(movies, movies by id, movies by director_id, and movies by ganre_id in progress)

--- app.py
+++ app.py
@@ -5,34 +5,62 @@ from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
 from config import db, app
+from models import Movie
+from schemas import MovieSchema, DirectorSchema, GenreSchema
+
+api = Api(app)
+
+# обьекты сериализации/десерализации
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+director_schema = DirectorSchema()
+directors_schema = DirectorSchema(many=True)
+genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
+
+# создаем неймспейсы
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('director')
+genre_ns = api.namespace('genre')
 
 
-# class Movie(db.Model):
-#     __tablename__ = 'movie'
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(255))
-#     description = db.Column(db.String(255))
-#     trailer = db.Column(db.String(255))
-#     year = db.Column(db.Integer)
-#     rating = db.Column(db.Float)
-#     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-#     genre = db.relationship("Genre")
-#     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-#     director = db.relationship("Director")
-#
-#
-# class Director(db.Model):
-#     __tablename__ = 'director'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
-#
-#
-# class Genre(db.Model):
-#     __tablename__ = 'genre'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
+@movie_ns.route("/")
+class MoviesViews(Resource):
+    def get(self):
+        all_movies = Movie.query.all()
+        return movies_schema.dump(all_movies), 200
 
 
+@movie_ns.route("/<int:id>")
+class MovieView(Resource):
+    def get(self, id: int):
+        try:
+            one_movie = Movie.query.filter(Movie.id == id).one()
+            return movie_schema.dump(one_movie), 200
+        except Exception:
+            return "", 404
+
+
+@movie_ns.route("/directors/<int:uid>")
+class DirectorsViews(Resource):
+    """Получаем все фильмы режиссера по его id """
+    def get(self, uid: int):
+        try:
+            movies_by_director = Movie.query.filter(Movie.director_id == uid).all()
+            return movies_schema.dump(movies_by_director), 200
+        except Exception:
+            return "", 404
+
+
+@movie_ns.route("/genres/<int:uid>")
+class GenresViews(Resource):
+    """Получаем все фильмы одного жанра по его id """
+    def get(self, uid: int):
+        try:
+            movies_by_genres = Movie.query.filter(Movie.genre_id == uid).all()
+            return movies_schema.dump(movies_by_genres), 200
+        except Exception:
+            return "", 404
 
 
 if __name__ == '__main__':
